7a8868c5d4b73bf387028fd14f17aabb902fcf1c
poek927-afk <poek927@newschool.edu>
2026-04-01 18:57:10 -0400

    back button added and fixed navigations

--- mainmenu/main.js
+++ mainmenu/main.js
@@ -1,11 +1,11 @@
-const destinations = {
-  "Equip":    "equip.html",
-  "Map":      "map.html",
-  "Quests":   "index.html",
-  "Settings": "settings.html"
-};
-
 const menuButtons = document.querySelectorAll('button.menu-btn');
-
 menuButtons.forEach(btn => {
   btn.addEventListener('click', () => {
+     menuButtons.forEach(b => b.classList.remove('active'));
+    btn.classList.add('active');
+    const destination = btn.dataset.href;
+    window.location.href = destination;
+ 
+  });
+});
+ 
